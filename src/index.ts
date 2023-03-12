0d244eeda7ff6595f96add721fca67475429d18a
adwd <ecdt.mn@gmail.com>
2023-03-12 21:00:04 +0900

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { createYoga, createSchema } from 'graphql-yoga';
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () => 'Hello from Yoga in a Bun app!',
+      },
+    },
+  }),
+});
+
+const server = Bun.serve(yoga);
+
+console.info(`Server is running on ${new URL(yoga.graphqlEndpoint, `http://${server.hostname}:${server.port}`)}`);
