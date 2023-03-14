f23d695a883a89ba32f4c0b384d0ca9e68059e6b
adwd <ecdt.mn@gmail.com>
2023-03-14 22:19:29 +0900

    add cache and sofa

--- bench/query.bench.ts
+++ bench/query.bench.ts
@@ -9,12 +9,48 @@ bench('setup', () => {
   const _yoga = createYoga({ schema, graphqlEndpoint });
 });
 
+bench('setup w/o cache', () => {
+  const schema = createSchema({ typeDefs, resolvers });
+  const graphqlEndpoint = '/test-graphql';
+  const _yoga = createYoga({
+    schema,
+    graphqlEndpoint,
+    parserCache: false,
+    validationCache: false,
+  });
+});
+
 (() => {
   const schema = createSchema({ typeDefs, resolvers });
   const graphqlEndpoint = '/test-graphql';
   const yoga = createYoga({ schema, graphqlEndpoint, logging: false });
 
   bench('query', async () => {
+    const _res = await yoga.fetch(`http://yoga/test-graphql`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          {
+            user(id: "1") {
+              id
+              fullName
+            }
+          }
+        `,
+      }),
+    });
+  });
+})();
+
+(() => {
+  const schema = createSchema({ typeDefs, resolvers });
+  const graphqlEndpoint = '/test-graphql';
+  const yoga = createYoga({ schema, graphqlEndpoint, logging: false, parserCache: false, validationCache: false });
+
+  bench('query w/o cache', async () => {
     const _res = await yoga.fetch(`http://yoga/test-graphql`, {
       method: 'POST',
       headers: {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import { createYoga, createSchema } from 'graphql-yoga';
 import { typeDefs } from './graphql/typeDefs.generated';
 import { resolvers } from './graphql/resolvers.generated';
+import { useSofaWithSwaggerUI } from '@graphql-yoga/plugin-sofa';
 
 const yoga = createYoga({
   schema: createSchema({ typeDefs, resolvers }),
+  plugins: [
+    useSofaWithSwaggerUI({
+      basePath: '/rest',
+      swaggerUIEndpoint: '/swagger',
+      servers: [
+        {
+          url: '/', // Specify Server's URL.
+          description: 'Development server',
+        },
+      ],
+      info: {
+        title: 'Example API',
+        version: '1.0.0',
+      },
+    }),
+  ],
 });
 const server = Bun.serve(yoga);
 
