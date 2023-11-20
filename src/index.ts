162d39ab6c3d59ecc38d03464b97e92066ae8c73
adwd <ecdt.mn@gmail.com>
2023-11-20 21:34:06 +0900

    remove sofa

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,10 @@ import { createYoga, createSchema } from 'graphql-yoga';
 import { typeDefs } from './graphql/typeDefs.generated';
 import { resolvers } from './graphql/resolvers.generated';
 import { GraphQLContext, createContext } from './context';
-// import { useSofa } from '@graphql-yoga/plugin-sofa';
 
 const yoga = createYoga({
   schema: createSchema<GraphQLContext>({ typeDefs, resolvers }),
   context: createContext,
-  plugins: [
-    // useSofa({
-    //   basePath: '/rest',
-    //   swaggerUI: {
-    //     endpoint: '/swagger',
-    //   },
-    // }),
-  ],
 });
 
 const server = Bun.serve(yoga);
